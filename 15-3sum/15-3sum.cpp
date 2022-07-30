0e6fadb2243d6aac74dad4e121682e924b8a348b
Vandana Yadav <85009521+yvandana12@users.noreply.github.com>
2022-07-30 08:58:18 +0530

    Time: 147 ms (52.77%), Space: 23.4 MB (33.91%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,35 +5,35 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-2;i++)
         {
-            if(i==0||nums[i]!=nums[i-1])
+            int s=i+1;
+            int e=nums.size()-1;
+            while((i+1)<nums.size()-2&&nums[i]==nums[i+1])
             {
-                int s=i+1;
-                int e=nums.size()-1;
-
-                while(s<e)
-                {
-                    if(nums[s]+nums[e]==(-nums[i]))
-                    {
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[s]);
-                        temp.push_back(nums[e]);
-                        res.push_back(temp);
-                        while(s<e&&nums[s]==nums[s+1]) s++;
-                        while(s<e&&nums[e]==nums[e-1]) e--;
-                        s++;
-                        e--;
-                    }
-                    else if(nums[s]+nums[e]>(-nums[i]))
-                    {
-                        e--;
-                    }
-                    else
-                    {
-                        s++;
-                    }
+                i++;
             }
-        }
+            while(s<e)
+            {
+                if(nums[s]+nums[e]==(-nums[i]))
+                {
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[s]);
+                    temp.push_back(nums[e]);
+                    res.push_back(temp);
+                    while(s<e&&nums[s]==nums[s+1]) s++;
+                    while(s<e&&nums[e]==nums[e-1]) e--;
+                    s++;
+                    e--;
+                }
+                else if(nums[s]+nums[e]>(-nums[i]))
+                {
+                    e--;
+                }
+                else
+                {
+                    s++;
+                }
+            }
         }
         return res;
     }
